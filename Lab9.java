3f211e33530998e74322bb86cc51b2bb972d25d1
Asif Rasheed <49068949+asifrasheed6@users.noreply.github.com>
2019-11-10 19:35:31 +0400

    Add files via upload

--- Lab9.java
+++ Lab9.java
@@ -27,110 +27,22 @@ import javax.swing.JPanel;
 public class Lab9 {
     public static void main(String[] args) {
          JFrame frame = new Frame(); 
+         frame.setVisible(true);
     }
 }
 
-//Ellipse is the Shape selected by default when the application launches
-//Blue is the Color selected by default when the application launches
-//User needs to press control in order to avoid accidental shortcuts
-
-class Frame extends JFrame{
-    private JPanel buttonPanel = new JPanel(); //Button Panel
-    private Dimension size = Toolkit.getDefaultToolkit().getScreenSize(); //Display Resolution
-    ArrayList<Shapes> shapes = new ArrayList<Shapes>(); //Contains list of shapes drawn
-    boolean ellipse = true; 
-    boolean rectangle = false;
-    Color color = Color.blue; //Fill Color, blue by default
+class DrawArea extends JPanel{
+    private ArrayList<Shapes> shapes = new ArrayList<Shapes>(); //Contains list of shapes drawn
+    private boolean ellipse = true; 
+    private boolean rectangle = false;
+    private Color color = Color.blue; //Fill Color, blue by default
+    private int iniX, iniY;
+    private boolean isFirst = true;
     
-    Frame(){ 
-        /*<-----JFrame----->*/
-        this.setSize(size.width/2, size.height/2);
-        this.setTitle("Michelangelo v1.0");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setFocusable(true);
-        
-        /*<-----Adding the buttons----->*/
-        JButton ellipseButton = new JButton("Ellipse");
-        ellipseButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                rectangle = false;
-                ellipse = true;
-                deselectAll();
-                requestFocusInWindow();
-            }
-        });
-        JButton rectangleButton = new JButton("Rectangle");
-        rectangleButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ellipse = false;
-                rectangle = true;
-                deselectAll();
-                requestFocusInWindow();
-            }
-        });
-        JButton blueButton = new JButton("Blue");
-        blueButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               color = Color.BLUE;
-               for(Shapes s: shapes)
-                    if(s.getColor()!=color)
-                        if(s.changeColor(color))
-                            repaint();
-               requestFocusInWindow();
-            }
-        });
-        JButton greenButton = new JButton("Green");
-        greenButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                color = Color.green;
-                for(Shapes s: shapes)
-                    if(s.getColor()!=color)
-                        if(s.changeColor(color))
-                            repaint();
-                requestFocusInWindow();
-            }
-        });
-        JButton magentaButton = new JButton("Magenta");
-        magentaButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                color = Color.magenta;
-                for(Shapes s: shapes)
-                    if(s.getColor()!=color)
-                        if(s.changeColor(color))
-                            repaint();
-                requestFocusInWindow();
-            }
-            
-        });
-        JButton deleteButton = new JButton("Delete");
-        deleteButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                callDelete();
-                requestFocusInWindow();
-            }
-        });
-        
-        buttonPanel.add(ellipseButton);
-        buttonPanel.add(rectangleButton);
-        buttonPanel.add(blueButton);
-        buttonPanel.add(greenButton);
-        buttonPanel.add(magentaButton);
-        buttonPanel.add(deleteButton);
-        
-        buttonPanel.setBackground(Color.white);
-        
-        this.add(buttonPanel);
-        
+    public DrawArea(){
+        /*<-----------Mouse Controllers---------->*/
         /*<-----Mouse Listener----->*/
         this.addMouseListener(new MouseAdapter(){
-            int iniX, iniY;
-            
             @Override
             public void mouseClicked(MouseEvent e){
                 if(e.getClickCount()==2){
@@ -149,96 +61,49 @@ class Frame extends JFrame{
             public void mousePressed(MouseEvent e){
                 /*Finding the X point and the Y point when the user start to drag*/
                 iniX = e.getX();
-                iniY = e.getY(); 
+                iniY = e.getY();
             }
+            
             @Override
             public void mouseReleased(MouseEvent e){
-                int finX = e.getX();
-                int finY = e.getY();
-                //If Ellipse is selected, adds ellipse to the ArrayList
-                //If Rectangle is selected, adds rectangle to the ArrayList
-                /*
-                There are two conditions for adding the shape.
-                Also repaint() is called only when an object is added.
-                This is to fix an animation issue caused on click.
-                */
-                if(ellipse & finX != iniX) 
-                {
-                    shapes.add(new Shapes(new Ellipse2D.Double(iniX, iniY, finX-iniX, finY-iniY), color));
-                    deselectAll();
-                    shapes.get(shapes.size()-1).toggleOutline();
-                    repaint();
-                }
-                if(rectangle & finY != iniY) 
-                {
-                    shapes.add(new Shapes(new Rectangle(iniX, iniY, finX-iniX, finY-iniY),color));
-                    deselectAll();
-                    shapes.get(shapes.size()-1).toggleOutline();
-                    repaint();
-                }
+                isFirst = true;
+                shapes.get(shapes.size()-1).toggleOutline();
+                repaint();       
             }
             
         });
         
-        /*<-----Keyboard Shortcuts----->*/
-        this.addKeyListener(new KeyAdapter(){
+        /*<-----MouseMotion Listener----->*/
+        this.addMouseMotionListener(new MouseAdapter(){
             @Override
-            public void keyPressed(KeyEvent e)
-            {
-                    switch(e.getKeyChar())
-                    {
-                        case 'b'|'B':
-                            color = Color.BLUE;
-                            for(Shapes s: shapes)
-                                if(s.getColor()!=color)
-                                    if(s.changeColor(color))
-                                        repaint();
-                            break;
-                        case KeyEvent.VK_DELETE:
-                            callDelete();
-                            break;
-                        case 'e'|'E':
-                            deselectAll();
-                            ellipse = true;
-                            rectangle = false;
-                            break;
-                        case 'g'|'G':
-                            color = Color.GREEN;
-                            for(Shapes s: shapes)
-                                if(s.getColor()!=color)
-                                    if(s.changeColor(color))
-                                        repaint();
-                            break;
-                        case 'm'|'M':
-                            color = Color.MAGENTA;
-                            for(Shapes s: shapes)
-                                if(s.getColor()!=color)
-                                    if(s.changeColor(color))
-                                        repaint();
-                            break;
-                        case 'r'|'R':
-                            deselectAll();
-                            rectangle = true;
-                            ellipse = false;
-                            break;
-                    }
+            public void mouseDragged(MouseEvent e){
+                int finX = e.getX();
+                int finY = e.getY();
+                deselectAll();
+
+                if(!isFirst) shapes.remove(shapes.size()-1);
+  
+                if(ellipse)
+                {
+                    shapes.add(new Shapes(new Ellipse2D.Double(Math.min(iniX, finX), Math.min(iniY, finY), Math.abs(finX-iniX), Math.abs(finY-iniY)),color));
+                    isFirst = false;
+                    repaint();
+                }
                     
-                    if(e.getKeyCode()==KeyEvent.VK_LEFT)
-                        selectPrevious();
-                    if(e.getKeyCode()==KeyEvent.VK_RIGHT)
-                        selectNext();
-                    if(e.getKeyCode()==KeyEvent.VK_ESCAPE)
-                        deselectAll();
+                if(rectangle)
+                {
+                    shapes.add(new Shapes(new Rectangle(Math.min(iniX, finX), Math.min(iniY, finY), Math.abs(finX-iniX), Math.abs(finY-iniY)),color));
+                    isFirst = false;
+                    repaint();
+                }             
             }
-        });
-        
-        this.setVisible(true);
+                
+        });   
     }
-    
     @Override
-    public void paint(Graphics g){
-        super.paint(g);
-        this.getContentPane().setBackground(Color.white); //Canvas Background is white
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        this.setBackground(Color.WHITE);
         
         /*<-----Drawing the shapes----->*/
         Graphics2D g2 = (Graphics2D) g;
@@ -290,6 +155,163 @@ class Frame extends JFrame{
                 s.toggleOutline();
                 repaint();
             }
+    }
+    
+    public void selectRectangle(){
+        rectangle = true;
+        ellipse = false;
+        deselectAll();
+    }
+    
+    public void selectEllipse(){
+        rectangle = false;
+        ellipse = true;
+        deselectAll();
+    }
+    
+    public void setColor(Color color){
+        this.color = color;
+    }
+    
+    public void recolor(){
+        for(Shapes s: shapes)
+            if(s.changeColor(color))
+                repaint();
+    }
+    
+}
+
+//Ellipse is the Shape selected by default when the application launches
+//Blue is the Color selected by default when the application launches
+//User needs to press control in order to avoid accidental shortcuts
+
+class Frame extends JFrame{
+    private JPanel buttonPanel = new JPanel(); //Button Panel
+    private Dimension size = Toolkit.getDefaultToolkit().getScreenSize(); //Display Resolution
+    private DrawArea draw;
+    Frame(){ 
+        /*<-----JFrame----->*/
+        this.setSize(size.width/2, size.height/2);
+        this.setTitle("Michelangelo v1.1");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setFocusable(true);
+        draw = new DrawArea();
+        draw.setBackground(Color.WHITE);
+        draw.setPreferredSize(size);
+     
+        /*<-----Adding the buttons----->*/
+        JButton ellipseButton = new JButton("Ellipse");
+        ellipseButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                draw.selectRectangle();
+                requestFocusInWindow();
+            }
+        });
+        JButton rectangleButton = new JButton("Rectangle");
+        rectangleButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                draw.selectEllipse();
+                requestFocusInWindow();
+            }
+        });
+        JButton blueButton = new JButton("Blue");
+        blueButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               draw.setColor(Color.BLUE);
+               draw.recolor();
+               requestFocusInWindow();
+            }
+        });
+        JButton greenButton = new JButton("Green");
+        greenButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                draw.setColor(Color.green);
+                draw.recolor();
+                requestFocusInWindow();
+            }
+        });
+        JButton magentaButton = new JButton("Magenta");
+        magentaButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                draw.setColor(Color.magenta);
+                draw.recolor();
+                requestFocusInWindow();
+            }
+            
+        });
+        JButton deleteButton = new JButton("Delete");
+        deleteButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                draw.callDelete();
+                requestFocusInWindow();
+            }
+        });
+        
+        buttonPanel.add(ellipseButton);
+        buttonPanel.add(rectangleButton);
+        buttonPanel.add(blueButton);
+        buttonPanel.add(greenButton);
+        buttonPanel.add(magentaButton);
+        buttonPanel.add(deleteButton);
+        
+        /*<-----Keyboard Shortcuts----->*/
+        this.addKeyListener(new KeyAdapter(){
+            @Override
+            public void keyPressed(KeyEvent e)
+            {
+                    switch(e.getKeyChar())
+                    {
+                        case 'b'|'B':
+                            draw.setColor(Color.BLUE);
+                            draw.recolor();
+                            requestFocusInWindow();
+                            break;
+                        case KeyEvent.VK_DELETE:
+                            draw.callDelete();
+                            break;
+                        case 'e'|'E':
+                            draw.selectEllipse();
+                            requestFocusInWindow();
+                            break;
+                        case 'g'|'G':
+                            draw.setColor(Color.GREEN);
+                            draw.recolor();
+                            requestFocusInWindow();
+                            break;
+                        case 'm'|'M':
+                            draw.setColor(Color.MAGENTA);
+                            draw.recolor();
+                            requestFocusInWindow();
+                            break;
+                        case 'r'|'R':
+                            draw.selectRectangle();
+                            requestFocusInWindow();
+                            break;
+                    }
+                    
+                    if(e.getKeyCode()==KeyEvent.VK_LEFT)
+                        draw.selectPrevious();
+                    if(e.getKeyCode()==KeyEvent.VK_RIGHT)
+                        draw.selectNext();
+                    if(e.getKeyCode()==KeyEvent.VK_ESCAPE)
+                        draw.deselectAll();
+            }
+        });
+        
+        buttonPanel.setBackground(Color.white);
+        buttonPanel.setPreferredSize(new Dimension(size.width,40));
+        
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(buttonPanel);
+        mainPanel.add(draw);
+        
+        this.add(mainPanel);
     }
 }
 
